f04472bf891749e66193cd0849e156e9548d5e6f
jayesh <jayesh@indimedi.com>
2016-01-30 00:02:40 +0530

    jayesh

---
+++ test/src/main/java/com/indimedi/test/App.java
@@ -0,0 +1,26 @@
+package com.indimedi.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private String userName;
+	private String pwd;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+}
